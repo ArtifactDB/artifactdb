ad11b416f9e0233d62fde98248d7a6dba41e6c80
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-01-27 13:30:50 -0800

    Mention that any return value from downloadFun is acceptable.
    At least, from the perspective of this package. We don't use the
    contents of the files for further work, so callers should be free to
    return any value from downloadFun as long as they know how to use it.

--- src/getFile.js
+++ src/getFile.js
@@ -7,12 +7,9 @@ import * as gh from "./globalRequestHeaders.js";
  * @param {string} baseUrl - Base URL of the REST API.
  * @param {string} id - The full ArtifactDB identifier of the resource of interest.
  * @param {object} [options={}] - Optional parameters.
- * @param {?function} [options.downloadFun=null] - Function that accepts a single string containing a URL and returns either:
- *
- * - A string containing a path, for Node.js.
- * - An ArrayBuffer containing the contents of the file, for Node.js and browsers.
- *
- * Defaults to a call to `getFun`.
+ * @param {?function} [options.downloadFun=null] - Function that accepts a single string containing a URL and returns the resource at that URL.
+ * The return value may be of any type but is generally expected to be either an ArrayBuffer/Uint8Array of the file contents or (for Node.js only) a string to a path containing the file contents.
+ * By default, a request is performed using `getFun` and an ArrayBuffer is returned containing the contents of the file.
  * @param {?function} [options.getFun=null] - Function that accepts a single string containing a URL and returns a Response object.
  * Only used when `downloadFun` is not provided.
  * Defaults to the in-built `fetch` function with {@linkcode globalRequestHeaders}.
